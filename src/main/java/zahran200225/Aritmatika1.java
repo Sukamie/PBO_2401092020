b2e071b2c6ac6294ab67ce37fc3e5522f6b7cf57
Sukamie <stud3nt4ccdarelilmi@gmail.com>
2025-02-20 15:36:17 +0700

    new project (20-02-2025)

---
+++ src/main/java/zahran200225/Aritmatika1.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package zahran200225;
+
+/**
+ *
+ * @author Suga
+ */
+public class Aritmatika1 {
+    public static void main( String[] args){
+        int i = 37;
+        int j = 42;
+        double x = 27.475;
+        double y = 7.22;
+        
+        System.out.println("Variable Value...");
+        System.out.println("   i = " + i);
+        System.out.println("   j = " + j);
+        System.out.println("   x = " + x);
+        System.out.println("   y = " + y);
+        
+        //penjumlahan
+        System.out.println("Adding....");
+        System.out.println("   i + j = " + (i + j));
+        System.out.println("   x + y = " + (x + y));
+        
+        //pengurangan
+        System.out.println("Substracting....");
+        System.out.println("   i + j = " + (i - j));
+        System.out.println("   x + y = " + (x - y));
+    }
+}
+
+
+        
