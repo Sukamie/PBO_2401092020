d25fb2c6d6bdf96813aa812e95895a18a4c02a9d
Suga <Suga@LittleSugar>
2025-02-20 14:54:32 +0700

    new project (20-02-2025)

---
+++ src/main/java/com/pnp/pbo_2401092020/PBO_2401092020.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pnp.pbo_2401092020;
+
+/**
+ *
+ * @author Suga
+ */
+public class PBO_2401092020 {
+
+    public static void main(String[] args) {
+        System.out.println("Belajar java");
+        System.out.println("Pemograman berorientasi objek");
+    }
+}
---
+++ src/main/java/zahran200225/Variabel1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package zahran200225;
+
+/**
+ *
+ * @author Suga
+ */
+public class Variabel1 {
+    public static void main(String[] args){
+        
+        boolean  result;
+        result = true;
+        char option;
+        option = 'C'; //menandai 'C' sebagai Option
+        double grade = 0.0;
+       
+       System.out.println(result);
+       System.out.println(option);
+       System.out.println(grade);
+    }
+}
