31cddd588cf9f3c22494ae0bb71264c705e6a515
Sukamie <stud3nt4ccdarelilmi@gmail.com>
2025-03-05 07:39:27 +0700

    update tugas (04-03-2025)

---
+++ src/main/java/latihan040225/Tugas4113.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihan040225;
+
+/**
+ *
+ * @author Suga
+ */
+public class Tugas4113 {
+    public static void main(String[] args){
+        
+        int num1 = 10;
+        int num2 = 23;
+        int num3 = 5;
+        int num4 = 0;
+        int result = 0;
+        
+        num4 = (num1 > num2)? num1 : num2;
+        result = (num4 > num3)? num4 : num3;
+        
+        System.out.println("Angka terbesar adalah : " + result);
+    }
+}
---
+++ src/main/java/latihan040225/Tugas4114.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihan040225;
+
+/**
+ *
+ * @author Suga
+ */
+public class Tugas4114 {
+    public static void main(String[] args){
+        //soal 1 :a/b^c^d-e+f-g*h+1
+        //jawaban 1 : (a/((b^c)^d))-e+f-(g*h)+1
+        
+        //soal 2 : 3*10*2/15-2+4^2^2
+        //jawaban 2 : (((3*10)*2)/15)-2+((4^2)^2)
+        
+        //soal 3 : r^s*t/u-v+w^x-y++
+        //jawaban 3 : (((r^s)*t)/u)-v+(w^x)-y++ 
+    }
+}
