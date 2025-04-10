09017858db03ae703376b4ee8ce89ba82205fe09
Sukamie <stud3nt4ccdarelilmi@gmail.com>
2025-04-10 10:09:31 +0700

    update tugas (10-04-25)

---
+++ src/main/java/zahran100425/StudentExample.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package zahran100425;
+
+/**
+ *
+ * @author Suga
+ */
+public class StudentExample {
+    public static void main(String[] args){
+        Student student = new Student();
+        System.out.println("Nama:" + student.getName());
+        System.out.println("Kota:" + student.address);
+    }
+}
